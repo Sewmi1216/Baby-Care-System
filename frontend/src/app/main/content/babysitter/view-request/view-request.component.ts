057fe1345b87037e9bed0040fdbb690856fc852f
Tharushi <ththarushi3@gmail.com>
2023-07-23 16:42:49 +0530

    Fix: request

--- frontend/src/app/main/content/babysitter/view-request/view-request.component.ts
+++ frontend/src/app/main/content/babysitter/view-request/view-request.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-view-request',
   templateUrl: './view-request.component.html',
-  styleUrls: ['./view-request.component.css']
+  styleUrls: ['./view-request.component.css'],
 })
 export class ViewRequestComponent {
-    title = "Request"
+
 }
