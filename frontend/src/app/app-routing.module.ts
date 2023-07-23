861672bbdc77c246165293c2cf7463e2c85f1001
Tharushi <ththarushi3@gmail.com>
2023-07-23 17:45:09 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
+import { ViewRequestComponent } from './main/content/babysitter/view-request/view-request.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'babysitter-view-request',
+    component: ViewRequestComponent,
   },
   ]
 
