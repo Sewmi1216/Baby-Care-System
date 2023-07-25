fdb9daa8493017adbeecb2449672e4c2d1903e8e
Tharushi <ththarushi3@gmail.com>
2023-07-25 19:14:08 +0530

    Completed create component and routing those.

---
+++ frontend/src/app/main/content/babysitter/babysitter-myprofile/babysitter-myprofile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-babysitter-myprofile',
+  templateUrl: './babysitter-myprofile.component.html',
+  styleUrls: ['./babysitter-myprofile.component.css']
+})
+export class BabysitterMyprofileComponent {
+
+}
