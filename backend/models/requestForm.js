5bafbf824a0a0a8bd52c79a6ef0ab3e13ca7bc85
Tharushi <ththarushi3@gmail.com>
2023-08-05 15:45:56 +0530

    Create a model for request form

---
+++ backend/models/requestForm.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
+const requestFormSchema = new schema({
+    workExpectation: [{
+        date: {
+            type: Date,
+            required: true
+        },
+        fromTime: {
+            type: String,
+            required: true
+        },
+        toTime: {
+            type: String,
+            required: true
+        }
+    }],
+    NumberofBabies: {
+        type: Number,
+        required: true
+    },
+    babyDetails: {
+        age: {
+            type: Number,
+            required: true
+        },
+        gender: {
+            type: String,
+            // enum: ['male', 'female'],
+            required: true
+        }
+    },
+    SpecialNeeds: {
+        type: String,
+        required: true,
+    }
+})
+
+const RequestForm = mongoose.model("RequestForm", requestFormSchema);
+
+module.exports = RequestForm;
+
+/* 
+    const newRequestFormData = {
+      workExpectation: [
+        {
+          date: new Date('2023-08-06'),
+          fromTime: '10:00 AM',
+          toTime: '3:00 PM',
+        },
+        {
+          date: new Date('2023-08-08'),
+          fromTime: '2:30 PM',
+          toTime: '4:00 PM',
+        },
+      ],
+      NumberofBabies: 1,
+      babyDetails: {
+        age: 1,
+        gender: 'male',
+      },
+      SpecialNeeds: 'None',
+    };
+*/
