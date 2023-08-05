f720ea9beda9e5e2b4c4398331b956c689bbc48c
Tharushi <ththarushi3@gmail.com>
2023-08-05 15:24:00 +0530

    Create a model for baby

--- backend/models/baby.js
+++ backend/models/baby.js
@@ -1,1 +1,22 @@
-requ
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
+const babySchema = new schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    gender: {
+        trpe: String,
+        required: true
+    }
+})
+
+const Baby = mongoose.model("Baby", babySchema);
+
+module.exports = Baby;
