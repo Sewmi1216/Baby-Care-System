96f922e2bf92cbcc427d70eee3f4d5796adde260
Sewmi <sewmithimaya248@gmail.com>
2023-08-08 03:57:08 +0530

    live

--- frontend/src/app/main/content/parent/live-stream/live-stream.component.ts
+++ frontend/src/app/main/content/parent/live-stream/live-stream.component.ts
@@ -14,12 +14,15 @@ interface ObjectPrediction {
   styleUrls: ['./live-stream.component.css']
 })
 export class LiveStreamComponent implements AfterViewInit {
+  @ViewChild('liveView', { static: true }) liveView!: ElementRef;
   @ViewChild('videoElement') videoElement!: ElementRef;
   @ViewChild('canvasElement') canvasElement!: ElementRef;
 
   // private socket!:Socket;
   private model: any; // Store the COCO-SSD model
   private stream!: MediaStream;
+  private children: any[] = [];
+
 
 
   constructor(private renderer: Renderer2) {
@@ -57,55 +60,44 @@ export class LiveStreamComponent implements AfterViewInit {
 
   async analyzeCameraFrames() {
     const video = this.videoElement.nativeElement;
+    const liveView = this.liveView.nativeElement as HTMLElement;
 
     if (video.readyState === 4 && video.videoWidth > 0 && video.videoHeight > 0) {
       const predictions: ObjectPrediction[] = await this.model.detect(video);
-      const canvas: HTMLCanvasElement = this.renderer.createElement('canvas');
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      const ctx = canvas.getContext('2d');
 
-      if (ctx) {
-        // Draw the video frame on the canvas
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-        // Draw bounding boxes
-        predictions.forEach(prediction => {
-          if (prediction.class === 'person') {
-            const [x, y, width, height] = prediction.bbox;
-            ctx.beginPath();
-            ctx.rect(x, y, width, height);
-            ctx.strokeStyle = 'red';
-            ctx.lineWidth = 2;
-            ctx.fillStyle = 'red';
-            ctx.globalAlpha = 0.2;
-            ctx.fill();
-            ctx.stroke();
-            ctx.fillStyle = 'white';
-            ctx.globalAlpha = 1;
-            ctx.font = '16px Arial';
-            ctx.fillText('Person', x, y - 5);
-          }
-        });
-
-        // Replace the video stream with the canvas for display
-        this.renderer.removeChild(video.parentNode, video);
-        this.renderer.appendChild(video.parentNode, canvas);
+      // Clear previous highlighting
+      for (const child of this.children) {
+        this.renderer.removeChild(this.liveView.nativeElement, child);
       }
+      this.children.splice(0);
 
-      const babyDetection = predictions.find(prediction => prediction.class === 'person');
-      if (!babyDetection) {
-        // Trigger alert logic and WebSocket event here
-        // You can emit a WebSocket event to notify the backend
-        // this.sendAlertToParent();
+      // Loop through predictions and draw bounding boxes
+      for (let n = 0; n < predictions.length; n++) {
+        if (predictions[n].score > 0.66) {
+          const p = this.renderer.createElement('p');
+          this.renderer.setProperty(p, 'innerText', predictions[n].class + ' - with ' + Math.round(parseFloat(String(predictions[n].score)) * 100) + '% confidence.');
+          this.renderer.setStyle(p, 'margin-left', predictions[n].bbox[0] + 'px');
+          this.renderer.setStyle(p, 'margin-top', (predictions[n].bbox[1] - 10) + 'px');
+          this.renderer.setStyle(p, 'width', (predictions[n].bbox[2] - 10) + 'px');
+          // You can continue setting other styles for the paragraph element
+
+          const highlighter = this.renderer.createElement('div');
+          this.renderer.setAttribute(highlighter, 'class', 'highlighter');
+          this.renderer.setStyle(highlighter, 'left', predictions[n].bbox[0] + 'px');
+          this.renderer.setStyle(highlighter, 'top', predictions[n].bbox[1] + 'px');
+          this.renderer.setStyle(highlighter, 'width', predictions[n].bbox[2] + 'px');
+          this.renderer.setStyle(highlighter, 'height', predictions[n].bbox[3] + 'px');
+          // You can continue setting other styles for the highlighter element
+
+          this.renderer.appendChild(this.liveView.nativeElement, highlighter);
+          this.renderer.appendChild(this.liveView.nativeElement, p);
+          this.children.push(highlighter);
+          this.children.push(p);
+        }
       }
     }
 
     requestAnimationFrame(() => this.analyzeCameraFrames());
-  }
-
-  sendAlertToParent(){
-    console.log('no person')
   }
 
 }
