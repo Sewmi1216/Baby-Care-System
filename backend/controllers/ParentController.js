fd8cd76049254ee27ca8bbd1a1ee7a6b4dd7428f
Tharushi <ththarushi3@gmail.com>
2023-08-09 19:14:23 +0530

    fix errors

--- backend/controllers/ParentController.js
+++ backend/controllers/ParentController.js
@@ -146,6 +146,33 @@ const deleteRequestForm = async (req, res) => {
         })
 }
 
+//complaint handling
+
+const addComplaints = async (req, res) =>{
+    const status = req.body.status;
+    const time = req.body.time;
+    const name = req.body.name;
+    const taskCompletedStatus = Boolean(req.body.taskCompletedStatus);
+    const remainderStatus = Boolean(req.body.remainderStatus);
+
+    const newTask = new Task({
+        status,
+        time,
+        name,
+        taskCompletedStatus,
+        remainderStatus,
+    });
+
+    await newTask.save()
+    .then((task) => {
+        res.status(200).send({status: "Task is added", task});
+    })
+    .catch((err) => {
+        console.log(err.message);
+        res.status(500).send({status: "Error with the task", error: err.message});
+    });
+};
+
 const addFeedback = async (req, res) => {
     //parent name
     const details = req.body.details;
--- backend/models/Complaint.js
+++ backend/models/Complaint.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const schema = mongoose.Schema;
 
 const complaintSchema = new schema({
     status: {
