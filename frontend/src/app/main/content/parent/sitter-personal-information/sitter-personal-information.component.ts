51a581b66956f7b3017fd946d4a7250c911d494b
Tharushi <ththarushi3@gmail.com>
2023-08-27 21:32:44 +0530

    Fixed: request form frontend

--- frontend/src/app/main/content/parent/sitter-personal-information/sitter-personal-information.component.ts
+++ frontend/src/app/main/content/parent/sitter-personal-information/sitter-personal-information.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class SitterPersonalInformationComponent {
 
+  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  times = ["12.00", "01.00", "02.00", "03.00", "04.00", "05.00", "06.00", "07.00", "08.00", "09.00", "10.00", "11.00"]
+  babyDetails: { age: number, gender: string }[] = [];
+
+
+  insertRow() {
+    this.babyDetails.push({ age: Number(''), gender: ''});
+  }
+
+  deleteRow() {
+    this.babyDetails.splice(1);
+  } 
 }
+
