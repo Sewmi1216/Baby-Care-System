c56e4c29ab7b765035a4e4ef4f075746264c187a
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2023-09-11 23:56:30 +0530

    forgot password

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const config = require("../config/auth.config");
 let User = require("../models/User")
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const nodemailer = require('nodemailer');
 
 const login = (req, res) => {
     const email = req.body.email;
@@ -42,7 +43,81 @@ const logout = (req, res) => {
     });
 }
 
+
+
+
+
+
+//forgot password
+
+const transporter = nodemailer.createTransport({
+    host: 'Gmail',
+    auth: {
+        user: '3rdyeargroupproject2023@gmail.com', // your email address
+        pass: 'ucsc@2023', // your email password
+    },
+});
+const sendResetPasswordEmail = (userEmail, resetToken) => {
+    const mailOptions = {
+        from: '3rdyeargroupproject2023@gmail.com', // your email address
+        to: userEmail, // recipient's email address
+        subject: 'Password Reset',
+        text: `Click the following link to reset your password: http://example.com/reset-password?token=${resetToken}`,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending password reset email:', error);
+        } else {
+            console.log('Password reset email sent:', info.response);
+        }
+    });
+};
+const forgotPassword = (req,res) =>{
+    const email = req.body.email;
+
+
+    User.findOne({email: email}).then((user) => {
+        if (!user) return res.status(400).json({msg: "User does not exist"});
+
+        // Generate a unique reset token (you can implement this as needed)
+        const resetToken = generateResetToken();
+const messege =`We have Recieved a password reset reqest.please use the below link to reset your password
+
+
+ ${resetURL}\n\n.This Reset Password link will be valid only for limited time`
+        // In a real application, you would save the reset token and expiration time to a database
+        // and associate it with the user's account for later verification.
+
+        // Send the reset password email to the user
+        sendResetPasswordEmail(email, resetToken);
+
+        // Respond to the client with a success message
+        res.status(200).json({ message: 'Password reset email sent successfully.' });
+    });
+};
+
+
+// Generate a unique reset token
+function generateResetToken() {
+    const crypto = require('crypto');
+    return crypto.randomBytes(20).toString('hex');
+
+
+
+}
+
+
+
+
+
+
+
 module.exports = {
     login,
-    logout
+    logout,
+    forgotPassword,
+    transporter,
+    sendResetPasswordEmail,
+
 }
+
