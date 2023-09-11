8c2e1fe7545b18c2a8de43cfbd8ab25e8c2f237e
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2023-09-12 00:39:52 +0530

    removed add baby details model -backend code

--- frontend/src/app/main/content/parent/baby-details/baby-details.component.ts
+++ frontend/src/app/main/content/parent/baby-details/baby-details.component.ts
@@ -5,35 +5,61 @@ import {NgToastService} from "ng-angular-popup";
 import {Router} from "@angular/router";
 import {ParentService} from "../../../../service/parent.service";
 import { CookieService } from 'ngx-cookie-service';
+import {string} from "@tensorflow/tfjs";
+import _default from "chart.js/dist/plugins/plugin.tooltip";
+import numbers = _default.defaults.animations.numbers;
+
+interface AddBabyForm{
+  firstName:string;
+  lastName:string;
+  age:number|null;
+  gender:string;
+  birthDate:Date|null;
+}
+
+
 @Component({
   selector: 'app-baby-details',
   templateUrl: './baby-details.component.html',
   styleUrls: ['./baby-details.component.css']
 })
-export class BabyDetailsComponent implements OnInit{
-  @ViewChild('addBabyForm', { static: true }) public addBabyForm!: NgForm;
+export class BabyDetailsComponent implements OnInit {
+  @ViewChild('addBabyForm', {static: true}) public addBabyForm!: NgForm;
 
-  babies: any[] = [];
-  baby = {
+  AddBabyForm = {
     firstName: '',
     lastName: '',
-    age:'',
-    gender:'',
-    birthDate:''
+    age: null,
+    gender: '',
+    birthDate: null,
+
+
   };
   private userId: any;
-
+  babies: any[] = [];
 
   constructor(
-    private parentService: ParentService, private toast: NgToastService, private router:Router,private cookieService: CookieService
-  ) {}
+    private parentService: ParentService,
+    private toast: NgToastService,
+    private router: Router,
+    private cookieService: CookieService
+  ) {
+  }
 
- // const accessToken = this.cookieService.get('access_token');
+  // const accessToken = this.cookieService.get('access_token');
+
+  firstName: string = '';
+  age: number | null = null;
+  birthDate: Date | null = null; // Initialize birthDate as null
+  lastName: string = ''; // Initialize lastName as an empty string
+  gender: string = ''; // Initialize gender as an empty string
+
   ngOnInit(): void {
-   // this.userId = this.parentService.getUserId();
-   // this.babyProfile = this.parentService.babyProfile;
+    // this.userId = this.parentService.getUserId();
+    // this.babyProfile = this.parentService.babyProfile;
     this.getBabies();
   }
+
   getBabies() {
     // @ts-ignore
     this.parentService.getBabies(JSON.parse(localStorage.getItem('user'))).subscribe(
@@ -49,23 +75,4 @@ export class BabyDetailsComponent implements OnInit{
   }
 
 
-
-
-  onSubmit() {
-    console.log("Submitting form...");
-    console.log(this.userId);
-    this.parentService.addBaby(this.baby, this.userId).subscribe(
-      (data) => {
-        console.log("Successfully");
-        this.router.navigate(['/parent/baby_details'])
-        this.toast.success({detail:"SUCCESS",summary:'Baby added successfully', position:'topCenter'});
-        console.log("Baby added successful:", data);
-
-      },
-      (err) => {
-        this.toast.error({detail:"ERROR",summary:err.error.message, position:'topCenter'});
-        console.log(`unsuccessful baby:${err}`, err);
-      }
-    );
-  }
 }
